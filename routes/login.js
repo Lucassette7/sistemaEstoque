1a16da569c5fb28f9b4c7681b994614b42aed3c2
LUCAS SETTE QUEIROZ <136368302+Lucassette7@users.noreply.github.com>
2024-12-06 16:54:57 -0300

    16:55

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,26 @@
 const express = require("express")
 const router = express.Router()
+const db = require('../database/db')
 
 router.get('/',(req, res)=>{
     res.render('login.ejs')
 }) 
+
+
+
+router.post('/salvarCadastro', (req, res)=>{
+    const {nome, telefone, email, cnpj, senha} = req.body
+
+    const query = 'INSERT INTO empresa (nome_empresa, cnpj, email, telefone, senha) VALUES (?, ?, ?, ?, ?)'
+
+    db.query(query, [nome, cnpj, email, telefone, senha], (erro, results)=>{
+        if(erro){
+            return res.status(404).json({success: false})
+        }
+        if(results.affectedRows > 0){
+            return res.json({success: true})
+        }
+    })
+})
 
 module.exports = router
