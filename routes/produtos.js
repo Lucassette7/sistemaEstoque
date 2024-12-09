f14baff7e69a5fb08c3168f153be53e2f817d831
LUCAS SETTE QUEIROZ <136368302+Lucassette7@users.noreply.github.com>
2024-12-09 16:42:22 -0300

    d

--- routes/produtos.js
+++ routes/produtos.js
@@ -39,4 +39,36 @@ router.get('/populaCategoria/:id_empresa', (req, res)=>{
         }
     })
 })
+
+
+//ADICIONA PRODUTO
+router.post('/adicionaProduto/:id_empresa', (req, res)=>{
+    const id_empresa = req.params.id_empresa
+    const {categoria, marca, produto, cor, valor, descricao} = req.body
+
+
+    const query = 'SELECT * FROM categoria WHERE categoria = ? and id_empresa = ?'
+
+    db.query(query, [categoria, id_empresa], (erro, results)=>{
+        if(erro){
+            return res.json('erro')
+        }
+        if(results.length > 0){
+            const id_categoria = results[0].id_categoria
+            
+            const query2 = 'INSERT INTO produto (id_categoria, id_empresa, produto, marca, cor, valor, descricao) VALUES (?, ?, ?, ?, ?, ?, ?)'
+            db.query(query2, [id_categoria, id_empresa, produto, marca, cor, valor, descricao], (erro, results)=>{
+                if(erro){
+                    return res.json('erro')
+                }
+                if(results.affectedRows > 0){
+                    return res.json({success: true})
+                }
+                else{
+                    return res.json({success: false})
+                }
+            })
+        }
+    })
+})
 module.exports = router
